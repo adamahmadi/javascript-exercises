e962c177c4f2fcea3394066b128d00e83e0fe4ed
adamahmadi <adam_ahmadi@hotmail.com>
2025-02-16 19:32:57 +0800

    Attempted sumAll and did not pass first test.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,11 @@
-const sumAll = function() {
+const sumAll = function (a , b) {
 
-};
+ let sum = 0;
+
+ for (let i = a; i <= b; i++) {
+    sum += 1;
+ } return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
